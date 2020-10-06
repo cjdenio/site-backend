ecf92cdaeb426328a2600f1f027bcc9d05591d63
Caleb Denio <cjdenio44@gmail.com>
2020-10-06 18:11:48 -0400

    🗃 Github URL field and dev database

--- config/database.js
+++ config/database.js
@@ -1,38 +1,41 @@
 const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
 
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-      },
-      options: {
-        ssl: false,
+if (process.env.NODE_ENV == "production") {
+  const config = parse(process.env.DATABASE_URL);
+
+  module.exports = ({ env }) => ({
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          username: config.user,
+          password: config.password,
+        },
+        options: {
+          ssl: false,
+        },
       },
     },
-  },
-});
-
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'postgres',
-//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-//       },
-//       options: {
-//         useNullAsDefault: true,
-//       },
-//     },
-//   },
-// });
+  });
+} else {
+  module.exports = ({ env }) => ({
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "sqlite",
+          filename: env("DATABASE_FILENAME", ".tmp/data.db"),
+        },
+        options: {
+          useNullAsDefault: true,
+        },
+      },
+    },
+  });
+}
