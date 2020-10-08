a5bb4d9588c8e37311d6a741faa0153c578601a7
Caleb Denio <cjdenio44@gmail.com>
2020-10-08 10:26:12 -0400

    🔒 Add comment to discourage hacking

--- config/server.js
+++ config/server.js
@@ -3,6 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
+      // btw this jwt secret isn't used in prod, so don't think about hacking my backend
       secret: env('ADMIN_JWT_SECRET', 'b730c4fa376452a873d1b5a7f7fa4237'),
     },
   },
