a3c5d1210b9ef8729674e4ae902a9530dcecd3ef
Caleb Denio <cjdenio44@gmail.com>
2021-03-11 09:39:48 -0500

    🎨 Format with Prettier

--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
       // btw this jwt secret isn't used in prod, so don't think about hacking my backend
-      secret: env('ADMIN_JWT_SECRET', 'b730c4fa376452a873d1b5a7f7fa4237'),
+      secret: env("ADMIN_JWT_SECRET", "b730c4fa376452a873d1b5a7f7fa4237"),
     },
   },
 });
--- extensions/users-permissions/config/jwt.js
+++ extensions/users-permissions/config/jwt.js
@@ -1,3 +1,3 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '2a35a386-1080-47fa-bba8-6909e9758de0'
+  jwtSecret: process.env.JWT_SECRET || "2a35a386-1080-47fa-bba8-6909e9758de0",
 };
