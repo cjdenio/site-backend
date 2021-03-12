2dd8d2a5e2088bfbb6beaa4fad7a2644f14834a6
Caleb Denio <cjdenio44@gmail.com>
2021-03-11 23:33:53 -0500

    ✨ Use MinIO for object storage

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,20 @@
-module.exports = ({ env }) => ({
-  upload: {
-    provider: "minio",
-    providerOptions: {},
-  },
-});
+if (process.env.NODE_ENV == "production") {
+  module.exports = ({ env }) => ({
+    upload: {
+      provider: "tp-minio",
+      providerOptions: {
+        accessKey: env("MINIO_ACCESS_KEY"),
+        secretKey: env("MINIO_SECRET_KEY"),
+        bucket: env("MINIO_BUCKET"),
+        endPoint: env("MINIO_ENDPOINT"),
+        port: 80,
+        useSSL: false,
+        isDocker: true,
+        folder: "assets",
+        host: env("MINIO_ENDPOINT"),
+      },
+    },
+  });
+} else {
+  module.exports = ({ env }) => {};
+}
